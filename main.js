aa24719a49b579d713302a08636a7ae880802033
sivaaravindh <sivaaravind262003@gmail.com>
2023-01-08 19:08:33 +0530

    index
    git commit -m index

---
+++ main.js
@@ -0,0 +1,33 @@
+// let arr=["1","text Usr", "25", "developerz"];
+// let obj={};
+
+// for(i=0;i<Array.length;i++){
+//       arr[i]=obj;
+// }
+// console.log(obj)
+
+// const a = { x:1, y:2 }; const b = a; b.x = 3;  console.log(a); console.log(b); 
+
+// let total = 20; switch (true){ case (total / 2 == 10 && total > 10): console.log(1); break; case (total != 20): console.log(2); break; case (total / 2 == 40 || total > 10): console.log(3); break; case (total + 20 == 40 && total != 20): console.log(4); break; case (total == 20 && total % 2 == 0): console.log(5); break; } 
+
+// let varOne = 15; while (varOne < 28){ console.log(varOne); varOne++; } 
+
+// var arr1 = "john".split(''); var arr2 = arr1.reverse(); var arr3 = "jones".split(''); arr2.push(arr3); console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(-1)); console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(-1)); 
+
+// console.log(1 +  "2" + "2"); console.log(1 +  +"2" + "2"); console.log(1 +  -"1" + "2"); console.log(+"1" +  "1" + "2"); console.log( "A" - "B" + "2"); console.log( "A" - "B" + 2); 
+
+// const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+// for (let i = 0; i < 10; i++) {
+//   setTimeout(() =>console.log(a[i]), 1000); 
+// }
+//  for (var i = 0; i < 10; i++) {
+  
+//   setTimeout(() =>console.log(a[i]), 1000); }
+
+function func1() { return { name:"Full Stack Tutorials", }; }  console.log(func1()); 
+
+function func2() {  return; { name: "Full Stack Tutorials"; }  } console.log(func2()); 
+
+console.log(typeof undefined); console.log(typeof null); console.log(typeof NULL); console.log(typeof typeof1);
+
+let varOne = 20; do { console.log(varOne); varOne++; } while (varOne < 20); 
